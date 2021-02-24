df17c6be3d11da10505a194a64d7cb972f5c8167
matemaynard99 <78230596+matemaynard99@users.noreply.github.com>
2021-02-23 18:17:32 -0600

    Update isNum.java

--- project/isNum.java
+++ project/isNum.java
@@ -1,4 +1,8 @@
-public class isNum {
+/*
+ * class to check if the a character is a number 
+ * @return : returns true or false depending on if char is a number
+ */
+public class is_Num {
 	//method to check if character  is a number
 	//simple method to see if char is between 1-9 and thus a number
 	static public boolean isNumber(char c) {
